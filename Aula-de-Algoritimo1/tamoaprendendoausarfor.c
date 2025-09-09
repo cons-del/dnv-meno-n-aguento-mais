9a8c5a41316759eece47c17d098afc53093062c7
Cauan Carvalho dos Santos <cauan.cons@gmail.com>
2025-09-08 21:28:54 -0300

    Create tamoaprendendoausarfor.c

---
+++ Aula-de-Algoritimo1/tamoaprendendoausarfor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+        
+int main(){
+    int valor;
+     
+     printf("Digite seu numero inteiro:");
+     scanf ("%d", &valor);
+     
+     
+     printf("n\tabuada do %d:\n", valor);
+     for (int contador = 0; contador <=10; contador++)
+     {
+     printf("numeros impares:")
+     printf("%d", valor);
+     }
+
+     }
+     
+  return 0;
+}
+        
